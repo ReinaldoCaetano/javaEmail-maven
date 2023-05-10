8b2d4c04198f786175a8ea6a05e4eb1fa091ebe2
Reinaldo Vitor Caetano <115084832+ReinaldoCaetano@users.noreply.github.com>
2023-05-10 15:44:54 -0300

    Criando projeto maven

---
+++ enviarEmail/src/main/java/br/com/proj/enviarEmail/App.java
@@ -0,0 +1,13 @@
+package br.com.proj.enviarEmail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
